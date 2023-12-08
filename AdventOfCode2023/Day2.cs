97be432ed97ff2d047b92fd9de30536cdeb9f16b
SolutiaKaiaH <kaia_hansen@solutiaconsulting.com>
2023-12-08 15:58:29 -0600

    Set up for day 2

---
+++ AdventOfCode2023/Day2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day2
+    {
+        public static void IndexValues(string filePath)
+        {
+            String line;
+
+            StreamReader sr = new StreamReader(filePath);
+
+            line = sr.ReadLine();
+
+
+        }
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 //Challenge 1
-using AdventOfCode;
+using AdventOfCode2023;
 using System.Globalization;
 using System.IO;
 
@@ -8,9 +8,14 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Starting...");
-        string filePath = "../../../inputs/dayOneP2Input.txt";
-        Day1.CalabrationValues(filePath);
+        //Console.WriteLine("Starting Day 1...");
+        //string filePath = "../../../inputs/dayOneP2Input.txt";
+        //Day1.CalabrationValues(filePath);
+
+
+        Console.WriteLine("Starting Day 2...");
+        string filePath = "../../../inputs/dayTwoInput.txt";
+        Day2.IndexValues(filePath);
 
     }
 }
